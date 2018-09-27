1360cccd89037078106018fa814865a119164980
vinayselestin5 <43402541+vinayselestin5@users.noreply.github.com>
2018-09-27 12:27:10 +0530

    Add files via upload

---
+++ func.py
@@ -0,0 +1,15 @@
+#using functions
+
+#reading values 
+a=12
+b=6
+#defining addition
+def add():
+	return a+b
+#defining subtracton
+def sub():
+	print("subtraction=",a-b)
+	return
+
+print("addition=",add())
+r=sub()
